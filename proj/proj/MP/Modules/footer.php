1f8719c814cc508a67dc1228c266eb0589b75a02
emaxv <max632116@gmail.com>
2025-04-11 11:02:31 +0300

    Add files via upload

---
+++ proj/proj/MP/Modules/footer.php
@@ -0,0 +1,15 @@
+        <br>Повестка дня:<br>
+        <button type="button" class="btn btn-primary btn-lg round" onClick="add()" >Добавить</button>
+        <div id="aim"></div>
+        <br>Выступили:<br>
+        <button type="button" class="btn btn-primary btn-lg round" onClick="addspeech()" >Добавить</button>
+        <div id="speech"></div>
+        <br>Решения кафедры<br>
+        <button type="button" class="btn btn-primary btn-lg round" onClick="soladd()" >Добавить</button>
+        <div id="sol"></div>
+        <br>
+        <input class="btn btn-danger btn-lg round pull-right" role="button" type="submit" value="Скачать" onClick="getName()" />
+        <br><br>
+    </form>
+</body>
+</html>
